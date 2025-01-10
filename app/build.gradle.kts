0e695bafcc3e9be90ed0576c269d1c3ce613c7ce
Serhii Tymoshenko <xo.tymoshenko@gmail.com>
2025-01-10 08:17:58 +0100

    Changed app name and icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "app.mariia.mariia"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "app.mariia.mariia"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
